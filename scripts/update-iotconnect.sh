f1ca5076bfa2f9e5f8a67c023e0557e65d965a65
Jordi <hokus04@gmail.com>
2024-03-26 14:01:26 +0100

    fix(update-ioconnect): fix update-iotconnect script

--- scripts/update-iotconnect.sh
+++ scripts/update-iotconnect.sh
@@ -1,19 +1,63 @@
 #!/bin/bash
+
+update_app() {
+    # Get the latest tag
+    latest_tag=$(curl https://api.github.com/repos/$repo/releases/latest | grep \"tag_name\" | cut -d : -f 2,3 | tr -d \", | xargs)
+    
+    echo "Updating IOTConnect to '${latest_tag}'..."
+    
+    # Fectch all tags
+    git fetch --all --tags --prune
+    # Reset
+    git reset --hard
+    # Check if latest brand exists
+    if [ "`git branch --list latest`" ];
+    then
+        # Move to master to be able to delete 'latest' branch
+        git checkout master
+        # Delete 'latest' branch
+        git branch -D latest;
+    fi
+    # Checkout latest tag to 'latest' branch
+    git checkout tags/"$latest_tag" -b latest
+
+    echo "done!"
+}
+
+post_update() {
+    echo "Performing post update actions..."
+    
+    # Change destination dir owner to pi:pi
+    sudo chown -R pi:pi "$dest_dir"
+    # Allow execution for scripts
+    sudo chmod +x "$dest_dir"/scripts/*.sh
+
+    # Update config files
+    cp "$dest_dir"/iotconnect/logging.live.conf iotconnect/logging.conf
+    cp "$dest_dir"/iotconnect/iotconnect.config.live.json iotconnect.config.json
+    sudo cp "$dest_dir"/scripts/*.sh ~
+    sudo chown -R pi:pi ~/*.sh
+    sudo chmod +x ~/*.sh
+    
+    echo "done!"
+}
+
+repo="hokus15/IOTConnect"
+dest_dir="/opt/IOTConnect"
+
+cd "$dest_dir" || exit
+
 echo 'Stopping IOTConnect service...'
 sudo service iotconnect stop
 echo 'done!'
-cd /opt/IOTConnect
-git fetch --tags
-tag=$(git tag -l | tail -n 1)
-echo "Updating IOTConnect to ${tag}..."
-git reset --hard
-git checkout master
-git branch --delete latest
-git checkout "$tag" -b latest
-echo 'done!'
-cp iotconnect/logging.live.conf iotconnect/logging.conf
-sudo chown -R pi /opt/IOTConnect
+
+update_app
+
+post_update
+
 sudo systemctl daemon-reload
+
 echo 'Starting IOTConnect service...'
 sudo service iotconnect start
-echo 'done!'
+
+echo 'Update complete!'
